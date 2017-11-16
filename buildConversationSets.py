03035f2ffd615f09b0b9c25e446df5d42efa0164
Tim Walsh <timwalsh300@gmail.com>
2017-11-16 15:40:52 -0500

    now capturing U's state as flagged if any of U's posts are flagged on day 0

--- buildConversationSets.py
+++ buildConversationSets.py
@@ -23,11 +23,12 @@ def getDate(inputStr):
     return date(int(postYear), int(postMonth), int(postDay))
 
 for n in range(1, 15):
+    # this will contain 'postUser date': [postLabel, replyText, nextPostLabel]
+    # where postLabel is 'flagged' if any post on that date was 'flagged', and
+    # nextPostLabel is the label of the user's next post after n days
+    conversationSets = {}
     print('working on ' + str(n) + '-day conversations')
-    conversationsFile = open(str(n) + 'dayConversations.txt', 'w')
     lineNumber = 0
-    numberOfSets = 0
-    statesCaptured = []
     for line in postsFile:
         if lineNumber == 63064:
             # stop when we get here (14 days before the end of the dataset) to make sure
@@ -41,18 +42,20 @@ for n in range(1, 15):
         else:
             postLabel = '?'
         postUser = postArray[2]
+        userState = postUser + ' ' + postDate.isoformat()
         # see if we've already built a conversation set for this user
-        # beginning on this day, and skip it if so
-        if (postUser + ' ' + postDate.isoformat()) in statesCaptured:
+        # beginning on this day, and skip it if so, but elevate label if appropriate
+        if userState in conversationSets:
+            if conversationSets[userState][0] == 'green' and postLabel == 'flagged':
+                conversationSets[userState][0] = 'flagged'
             lineNumber += 1
             continue
-        # if not, record the new user on this day
-        statesCaptured.append(postUser + ' ' + postDate.isoformat())
+        else: # if not, record the new user on this day
+            conversationSets[userState] = [postLabel, ' ', 'dummyLabel']
         # string to append text of replies to this user as we find them
         replyText = ''
         # increment this variable to look at posts beyond the target user post
         searchDistance = 1
-        repliesFound = 0
         # iterate through all subsequent posts for the next n days looking for replies
         while(True):
             potentialReply = postsFile[lineNumber + searchDistance].split('\t')
@@ -62,14 +65,15 @@ for n in range(1, 15):
                 searchDistance += 1
                 continue
             if postUser in potentialReply[3]:
-                repliesFound += 1
                 replyText = replyText + ' ' + potentialReply[4][:-1]
             searchDistance += 1
-        # only write a line to the file if we actually found a conversation
-        if repliesFound > 0:
-            numberOfSets += 1
-            # still need to find the target user's next post and append that label on here too
-            conversationsFile.write(postID + '\t' + postLabel + '\t' + replyText + '\n')
+        # still need to find the target user's next post and append that label on here too
+        conversationSets[userState][1] = replyText
         lineNumber += 1
-    conversationsFile.write('number of sets = ' + str(numberOfSets))
+    conversationsFile = open(str(n) + 'dayConversations.txt', 'w')
+    for key, value in conversationSets.items():
+        keyArray = key.split(' ')
+        # only write to the file if we found reply text and a nextPostLabel
+        if value[1] != '': # and value[2] != '?':
+            conversationsFile.write(keyArray[0] + '\t' + keyArray[1] + '\t' + value[0] + '\t' + value[1] + '\t' + value[2] + '\n')
     conversationsFile.close()
